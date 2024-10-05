268da9cb28902bb90a5047770e3973e5f44a647a
Julian Lasso <julianlasso1133109@correo.itm.edu.co>
2024-10-05 17:32:27 -0500

    feat: :sparkles: agrega efecto de desenfoque en el dashboard al mostrar modales

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -20,6 +20,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 
+import { ModalStateService } from '../../services/modal-state.service';
 import { HeaderComponent } from './header/header.component';
 import { MenuIconComponent } from './menu-icon/menu-icon.component';
 import { MenuComponent } from './menu/menu.component';
@@ -45,6 +46,7 @@ export class DashboardComponent implements OnInit, OnDestroy {
   private readonly authStorageService = inject(AuthStorageService);
   private readonly menuService = inject(MenuService);
   private readonly router = inject(Router);
+  private readonly modalStateService = inject(ModalStateService);
   private readonly buttonAddService = inject(ButtonAddService);
   public profile: Record<string, unknown>;
   public title: Signal<string>;
@@ -52,18 +54,22 @@ export class DashboardComponent implements OnInit, OnDestroy {
   private titleService: Subscription;
   private buttonAddVisibleService: Subscription;
   private buttonAddActionService: Subscription;
+  private modalStateVisibleService: Subscription;
   readonly buttonAddVisible: WritableSignal<boolean>;
   readonly buttonAddAction: WritableSignal<Function>;
+  blurEffect: WritableSignal<boolean>;
 
   constructor() {
     this.profile = {};
     this.title = signal('');
+    this.blurEffect = signal(false);
     this.storageService = new Subscription();
     this.titleService = new Subscription();
     this.buttonAddVisible = signal(false);
     this.buttonAddAction = signal(() => {});
     this.buttonAddVisibleService = new Subscription();
     this.buttonAddActionService = new Subscription();
+    this.modalStateVisibleService = new Subscription();
   }
 
   ngOnInit(): void {
@@ -92,6 +98,14 @@ export class DashboardComponent implements OnInit, OnDestroy {
         this.buttonAddAction.set(action);
       },
     });
+
+    this.modalStateVisibleService = this.modalStateService.state$.subscribe({
+      next: (visible: boolean) => {
+        this.blurEffect.set(visible);
+      },
+    });
+
+    this.blurEffect.set(this.modalStateService.state);
   }
 
   ngOnDestroy(): void {
@@ -99,6 +113,7 @@ export class DashboardComponent implements OnInit, OnDestroy {
     this.titleService.unsubscribe();
     this.buttonAddVisibleService.unsubscribe();
     this.buttonAddActionService.unsubscribe();
+    this.modalStateVisibleService.unsubscribe();
   }
 
   showData(): void {
