406751057e19be3b100f83f8d592ec1572332fe4
Julian Lasso <julianlasso1133109@correo.itm.edu.co>
2024-10-08 01:22:01 -0500

    feat: :sparkles: actualiza componente Search para usar SearchBarService y manejar placeholders dinámicamente en la búsqueda

--- src/app/components/dashboard/header/search/search.component.ts
+++ src/app/components/dashboard/header/search/search.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, signal, WritableSignal } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { Subscription } from 'rxjs';
+import { SearchBarService } from '../../../../services/search-bar.service';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss',
 })
-export class SearchComponent {}
+export class SearchComponent {
+  private readonly _searchBarService = inject(SearchBarService);
+  private _searchSubscription: Subscription;
+  readonly placeholder: WritableSignal<string>;
+
+  constructor() {
+    this.placeholder = signal('');
+    this._searchSubscription = new Subscription();
+  }
+
+  ngOnInit(): void {
+    this._searchSubscription = this._searchBarService.placeholder$.subscribe({
+      next: (placeholder) => this.placeholder.set(placeholder),
+    });
+  }
+
+  ngOnDestroy(): void {
+    this._searchSubscription.unsubscribe();
+  }
+
+  onSearch(search: string): void {
+    this._searchBarService.search = search;
+  }
+}
