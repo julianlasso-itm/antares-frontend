ac89573423dd7818aac2603514b271867bd9d065
Julian Lasso <julianlasso1133109@correo.itm.edu.co>
2024-10-09 00:01:44 -0500

    feat: :sparkles: agregar funcionalidad para limpiar el input de búsqueda cuando se cambia entre páginas

--- src/app/components/dashboard/header/search/search.component.ts
+++ src/app/components/dashboard/header/search/search.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, signal, WritableSignal } from '@angular/core';
+import { Component, ElementRef, inject, signal, ViewChild, WritableSignal } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { Subscription } from 'rxjs';
 import { SearchBarService } from '../../../../services/search-bar.service';
@@ -12,18 +12,28 @@ import { SearchBarService } from '../../../../services/search-bar.service';
   styleUrl: './search.component.scss',
 })
 export class SearchComponent {
+  @ViewChild('searchInput') searchInput!: ElementRef;
   private readonly _searchBarService = inject(SearchBarService);
   private _searchSubscription: Subscription;
+  private _clearSubscription: Subscription;
   readonly placeholder: WritableSignal<string>;
 
   constructor() {
     this.placeholder = signal('');
     this._searchSubscription = new Subscription();
+    this._clearSubscription = new Subscription();
   }
 
   ngOnInit(): void {
     this._searchSubscription = this._searchBarService.placeholder$.subscribe({
       next: (placeholder) => this.placeholder.set(placeholder),
+    });
+    this._clearSubscription = this._searchBarService.clear$.subscribe({
+      next: (clear) => {
+        if (clear) {
+          this.searchInput.nativeElement.value = '';
+        }
+      },
     });
   }
 
