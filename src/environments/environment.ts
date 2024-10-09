8ece969cf1df1740a3e772955df7444c953a827a
Julian Lasso <julianlasso1133109@correo.itm.edu.co>
2024-10-09 00:00:05 -0500

    feat: :sparkles: se añaden las url de los endpoints a nivel de variables de entorno

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,43 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.sofka.com.co',
+  apiUrl: 'https://antares.sofka.com.co',
+  endpoint: {
+    assessments: {
+      assessments: '/assessments/assessments',
+      configurationPerLevel: '/assessments/configuration-per-level',
+      configurations: '/assessments/configuration-levels',
+      domainAssessmentScores: '/assessments/domain-assessment-scores',
+      domainKnowledge: '/assessments/domain-knowledge',
+      domainKnowledgeLevels: '/assessments/domain-knowledge-levels',
+      domainQuestionsAnswers: '/assessments/domain-questions-answers',
+      levels: '/assessments/levels',
+      ratingScale: '/assessments/rating-scale',
+    },
+    humanResources: {
+      professionals: '/human-resources/professionals',
+    },
+    knowledgeGaps: {
+      gaps: '/knowledge-gaps/gaps',
+      notes: '/knowledge-gaps/notes',
+    },
+    projectsManagement: {
+      customers: '/projects-management/customers',
+      projects: '/projects-management/projects',
+      rolePerProfessional: '/projects-management/role-per-professional',
+      roles: '/projects-management/roles',
+      technologyPerRole: '/projects-management/technology-per-role',
+      technologyStack: '/projects-management/technology-stack',
+    },
+    security: {
+      roles: '/security/roles',
+      userPerRole: '/security/user-per-role',
+      users: '/security/users',
+    },
+    technologies: {
+      items: '/technologies/items',
+      types: '/technologies/types',
+    },
+  },
   googleId: {
     issuer: 'https://accounts.google.com',
     clientId:
